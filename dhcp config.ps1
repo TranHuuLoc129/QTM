6a457053465c75665c5d55c5483cbcc4e30e6ad0
Trần Hữu Lộc <113120646+TranHuuLoc129@users.noreply.github.com>
2023-10-04 16:21:41 +0700

    Add files via upload

---
+++ dhcp config.ps1
@@ -0,0 +1,15 @@
+﻿
+
+Install-WindowsFeature -Name DHCP -IncludeManagementTools
+
+Add-DhcpServerInDC -DnsName dhcp01.labtdtu.com -IPAddress 192.168.1.2
+
+#kiem tra lai => Get-DhcpServerInDC
+
+Add-DhcpServerv4Scope -Name LAN_A -StartRange 192.168.1.1 -EndRange 192.168.1.254 -SubnetMask 255.255.255.0 -LeaseDuration 02:00:00 -State Active
+
+#kiem tra lai => Get DhcpServerv4Scope
+
+Set-DhcpServerv4OptionValue -ScopeId 192.168.1.0 -DnsDomain labtdtu.com -DnsServer 192.168.1.2 -Router 192.168.1.1
+
+Add-DhcpServerv4ExclusionRange -ScopeId 192.168.1.0 -StartRange 192.168.1.1 -EndRange 192.168.1.100
